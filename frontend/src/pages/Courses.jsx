6748b8d1c9ec68da7937b066dc648bfbfacecbfb
Rohit Thimaya <66617953+Rohitthimaya@users.noreply.github.com>
2025-03-31 03:15:12 -0700

    Update Courses.jsx

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -48,7 +48,7 @@ function Courses() {
     }
 
     try {
-      const response = await fetch(`http://localhost:3000/api/courses/courses/delete/${courseId}`, {
+      const response = await fetch(`https://course-matcher-backend.onrender.com/api/courses/courses/delete/${courseId}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
